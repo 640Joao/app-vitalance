63e60c4b5ba841a187552fbb051e52f86665b855
João Elias <joanegreiros15@gmail.com>
2025-10-27 20:22:49 -0300

    corrigindo diretórios

--- backend/src/main/kotlin/com/vitalance/vitalance/dto/AuthDtos.kt
+++
@@ -1,7 +0,0 @@
-package com.vitalance.vitalance.dto
-
-data class RegisterRequest(
-    val name: String,
-    val email: String,
-    val password: String
-)
--- backend/src/main/kotlin/com/vitalance/vitalance/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.vitalance.vitalance.repository
-
-import com.vitalance.vitalance.model.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository: JpaRepository<User, Long> {
-    fun findByEmail(email: String): User?
-}
